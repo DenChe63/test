09d85c1aaac953f0e80a8c2eada94929a520327f
cherednik <cherednik@dot-dot.ru>
2021-04-14 16:09:01 +0300

    master add trash

--- www/test.dev/index.php
+++ www/test.dev/index.php
@@ -1,10 +1,22 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
-define('ROOT', dirname(__FILE__));
-require_once (ROOT.'/components/Router.php');
-require_once (ROOT.'/components/Db.php');
-$router = new Router();
-$router->run();
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
+require dirname(__DIR__).'/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
